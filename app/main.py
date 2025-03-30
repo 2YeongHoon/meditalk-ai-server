83a9b5970bc87d95820c421df64cd4cef3e8d3ae
yeonghoon.lee <djaakclsrn12@naver.com>
2025-03-30 23:58:39 +0900

    질병 매핑 서비스 추가

--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from app.routes import chatbot
+# from app.routes import chatbot
+from app.routes import disease
 
 app = FastAPI()
 
-app.include_router(chatbot.router)
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, Meditalk AI!"}
+# app.include_router(chatbot.router)
+app.include_router(disease.router)
